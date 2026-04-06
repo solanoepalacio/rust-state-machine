4693d4fb964bc37af19bbc099f597186fbb7f0f9
solanoe <solanoepalacio@gmail.com>
2026-04-06 17:27:08 -0300

    system pallet and runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
 mod balances;
 mod system;
 
+#[derive(Debug)]
+pub struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet,
+}
+
+impl Runtime {
+	fn new() -> Self {
+		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+}
+
 fn main() {
-	println!("Hello, world!");
+	let mut runtime = Runtime::new();
+
+	runtime.balances.set_balance(&"alice".to_string(), 100);
+
+	runtime.system.inc_block_number();
+
+	assert_eq!(runtime.system.block_number(), 1);
+
+	runtime.system.inc_nonce(&"alice".to_string());
+
+	let _res = runtime
+		.balances
+		.transfer("alice".to_string(), "bob".to_string(), 30)
+		.map_err(|e| eprintln!("{e}"));
+
+	runtime.system.inc_nonce(&"alice".to_string());
+
+	let _res2 = runtime
+		.balances
+		.transfer("alice".to_string(), "charlie".to_string(), 20)
+		.map_err(|e| eprintln!("{e}"));
+
+	println!("{runtime:#?}");
 }
--- src/system.rs
+++ src/system.rs
@@ -2,6 +2,8 @@ use std::collections::BTreeMap;
 
 /// This is the System Pallet.
 /// It handles low level state needed for your blockchain.
+
+#[derive(Debug)]
 pub struct Pallet {
 	/// The current block number.
 	block_number: u32,
@@ -25,25 +27,24 @@ impl Pallet {
 	}
 
 	pub fn inc_nonce(&mut self, who: &String) {
-        *self.nonce.entry(who.clone()).or_default() += 1;
+		*self.nonce.entry(who.clone()).or_default() += 1;
 	}
 }
 
-
-
 #[cfg(test)]
 mod test {
-    #[test]
-    fn init_system() {
-        let mut system = super::Pallet::new();
+	#[test]
+	fn init_system() {
+		let mut system = super::Pallet::new();
 
-        system.inc_block_number();();
-        system.inc_nonce(&"alice".to_string());
+		system.inc_block_number();
+		();
+		system.inc_nonce(&"alice".to_string());
 
-        assert_eq!(system.block_number(), 1);
+		assert_eq!(system.block_number(), 1);
 
-        assert_eq!(system.nonce.get("alice"), Some(&1));
+		assert_eq!(system.nonce.get("alice"), Some(&1));
 
-        assert_eq!(system.nonce.get("bob"), None);
-    }
+		assert_eq!(system.nonce.get("bob"), None);
+	}
 }
